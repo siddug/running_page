d60eaf4c88553ca134890997fdc457fbae204e8f
Siddhartha Gunti <siddhartha.gunti191@gmail.com>
2020-11-05 12:42:18 +0530

    init

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,8 +1,14 @@
 // const
-const MAPBOX_TOKEN = 'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
-const MUNICIPALITY_CITIES_ARR = ['北京市', '上海市', '天津市', '重庆市', '香港特别行政区', '澳门特别行政区'];
-const IS_CHINESE = true
+const MAPBOX_TOKEN =
+    "pk.eyJ1Ijoic2lkZ3VudGkiLCJhIjoiY2toM3Zva25pMDY2czJ0cXAydWx3cXI5aCJ9.7Wq6kqzZtIh_LGzr0nXsBQ";
+const MUNICIPALITY_CITIES_ARR = [
+    "北京市",
+    "上海市",
+    "天津市",
+    "重庆市",
+    "香港特别行政区",
+    "澳门特别行政区",
+];
+const IS_CHINESE = false;
 
-export {
-  MAPBOX_TOKEN, MUNICIPALITY_CITIES_ARR, IS_CHINESE
-};
+export { MAPBOX_TOKEN, MUNICIPALITY_CITIES_ARR, IS_CHINESE };
